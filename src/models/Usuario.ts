1828f90ab3b0d00ae311c7880cb89161a8a2b77e
vspeed21 <vtorresquintanilla0206@gmail.com>
2023-11-22 19:21:10 -0600

    add: password hashing with bcrypt

--- src/models/Usuario.ts
+++ src/models/Usuario.ts
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import { IUsuario } from '../interfaces/models';
+import bcrypt from 'bcrypt';
 
 const usuarioSchema = new mongoose.Schema({
   nombre: {
@@ -34,5 +35,13 @@ const usuarioSchema = new mongoose.Schema({
 });
 
 const Usuario = mongoose.model<IUsuario>('Usuario', usuarioSchema);
+
+usuarioSchema.pre('save', async function (next) {
+  if (!this.isModified(this.password)) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(20);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 export default Usuario;
